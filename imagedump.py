ebae8ce6c1a7f029032f5628c98c01b7fea34c67
vfsrfs <christian.presa-schnell@tum.de>
2019-03-12 16:20:55 +0100

    - Fix imports by DLL Name

--- imagedump.py
+++ imagedump.py
@@ -1,6 +1,9 @@
-import pefile
+import collections
 
+import pefile
+import os
 from unicorn.x86_const import *
+import struct
 
 
 class ImageDump(object):
@@ -23,11 +26,99 @@ class ImageDump(object):
             if s.Name in ntp:
                 self.set_protections(s, ntp[s.Name])
 
-    def fix_imports(self, pe):
+    # Returns list of addresses where search has been found
+    def find_occurences(self, binary, search):
+        occ = []
+        if isinstance(search, str):
+            search = bytes(search, 'ascii')
+        x = binary.find(search)
+        while x != -1:
+            occ.append(x)
+            x = binary.find(search, (x+1), len(binary))
+        return occ
+
+    def locate_ptr_to_occurences(self, binary, addresslist):
+        ptrs = []
+        possible_ptr = []
+        for a in addresslist:
+            ptrs.append(self.find_occurences(binary, struct.pack("I", a)))
+
+        for p1 in ptrs:
+            for p2 in p1:
+                possible_ptr.append(p2)
+        return possible_ptr
+
+    def search_offset_two(self, addrlist1, addrlist2, offset):
+        for a1 in addrlist1:
+            for a2 in addrlist2:
+                if a1 + offset == a2:
+                    return a1, a2
+        return None, None
+
+    # Fix imports by DLL Name
+    def fix_imports_by_dllname(self, pe, dllname_to_functionlist):  # TODO give options to user if other (valid) addresses are found
+        addr = pe.OPTIONAL_HEADER.DATA_DIRECTORY[1].VirtualAddress
+        pe.write(".unipacker_brokenimport.exe")
+        with open(".unipacker_brokenimport.exe", 'rb') as f:
+            b = f.read()
+
+        dllname_to_ptrs = []
+
+        for k, v in dllname_to_functionlist:
+            dllname_to_ptrs.append((k, super().locate_ptr_to_occurences(b, super().find_occurences(b, k))))
+
+        if len(dllname_to_ptrs) == 1 and len(dllname_to_ptrs[0][1]) == 1:
+            addr = dllname_to_ptrs[0][1]
+        elif len(dllname_to_ptrs) == 1:
+            # TODO Try Fix Imports by Imported Function Names
+            return False
+        else:
+            addrlist = dllname_to_ptrs[0][1]
+            addrlist2 = dllname_to_ptrs[1][1]
+
+            a1, a2 = self.search_offset_two(addrlist, addrlist2, 0x14)
+            if a1 is None and a2 is None:
+                return False
+
+            dllname_to_ptrs[0] = (dllname_to_ptrs[0][0], [a1])
+            dllname_to_ptrs[1] = (dllname_to_ptrs[1][0], [a2])
+
+            offset = 0x14
+
+            for i in range(len(dllname_to_ptrs)):
+                if i+1 < len(dllname_to_ptrs):
+                    cmp = dllname_to_ptrs[i][1][0]
+                    val = None
+                    for e in dllname_to_ptrs[i+1][1]:
+                        if cmp + 0x14 == e:
+                            val = e
+                    dllname_to_ptrs[i+1] = (dllname_to_ptrs[i+1][0], [val])
+
+            # select pointer
+            for i in range(len(dllname_to_ptrs)):
+                if addr > dllname_to_ptrs[i][1][0]:
+                    addr = dllname_to_ptrs[i][1][0]
+
+            pe.OPTIONAL_HEADER.DATA_DIRECTORY[1].VirtualAddress = addr - 0xC
+
+        os.remove(".unipacker_brokenimport.exe")
+
+    def fix_imports(self, uc, pe, dllname_to_functionlist):
+        pe.write(".unipacker_brokenimport.exe")
+        with open(".unipacker_brokenimport.exe", 'rb') as f:
+            b = f.read()
+
+        print(dllname_to_functionlist)
+
+        pe.OPTIONAL_HEADER.DATA_DIRECTORY[1].VirtualAddress = 0x60000
+
+
+        os.remove(".unipacker_brokenimport.exe")
         pass
 
     def dump_image(self, uc, base_addr, virtualmemorysize, apicall_handler, path="unpacked.exe"):
         ntp = apicall_handler.ntp
+        dllname_to_functionlist = apicall_handler.dllname_to_functionlist
         loaded_img = uc.mem_read(base_addr, virtualmemorysize + 0x3000)
         pe = pefile.PE(data=loaded_img)
 
@@ -46,7 +137,7 @@ class ImageDump(object):
         self.fix_section_mem_protection(pe, ntp)
 
         print("Fixing Imports...")
-        self.fix_imports(pe)
+        self.fix_imports(uc, pe, dllname_to_functionlist)
 
         pe.OPTIONAL_HEADER.CheckSum = pe.generate_checksum()
 
@@ -55,6 +146,6 @@ class ImageDump(object):
 
 
 class YZPackDump(ImageDump):
+    def fix_imports(self, uc, pe, dllname_to_functionlist):
+        super().fix_imports_by_dllname(pe, dllname_to_functionlist)
 
-    def fix_imports(self, pe):  # TODO this is only for the YZPack sample for testing purposes
-        pe.OPTIONAL_HEADER.DATA_DIRECTORY[1].VirtualAddress = 0x60000
