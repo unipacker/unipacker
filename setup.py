dcfa528e906f75069448e8a194c737ade3acfb3e
Samuel Hopstock <masrepus97@gmail.com>
2019-05-22 22:41:44 +0200

    Set description content type to markdown (PyPi wasn't able to parse it)

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     author_email='masrepus97@gmail.com',
     description='Automatic and platform-independent unpacker for Windows binaries based on emulation',
     long_description=README,
+    long_description_content_type='text/markdown',
     license='GPL-2.0',
     url='https://github.com/unipacker/unipacker',
     packages=find_packages(),
